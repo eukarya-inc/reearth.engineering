7688d6e24ed2679d3e2a00ab9b7c9b13f9bdc7b4
rot1024 <aayhrot@gmail.com>
2024-06-08 04:26:23 +0900

    feat: sst up post footer

--- src/customization/config.ts
+++ src/customization/config.ts
@@ -1,8 +1,13 @@
 import defaultConfig from "../config-default";
 import type { Config } from "../interfaces";
 
+const year = new Date().getFullYear();
+
 const config: Config = {
   ...defaultConfig,
+  footer: {
+    content: `©︎ ${year} Eukarya Inc., All Rights Reserved.`,
+  },
 };
 
 export default config;
