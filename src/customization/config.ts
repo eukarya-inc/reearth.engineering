f2da407ceaa036b04eec2734024f1e9f1fa9d69b
rot1024 <aayhrot@gmail.com>
2025-11-06 01:43:55 +0900

    update favicon and og image

--- src/customization/config.ts
+++ src/customization/config.ts
@@ -4,6 +4,10 @@ const year = new Date().getFullYear();
 
 const config: Config = {
   ...defaultConfig,
+  og: {
+    ...defaultConfig.og,
+    imagePadding: 50,
+  },
   footer: {
     content: `©︎ ${year} Eukarya Inc., All Rights Reserved.`,
   },
